060def209841d564e502a240433b5b694d4f9663
Mesut Çifci <mesutcifci97@gmail.com>
2021-03-11 23:47:04 +0300

    -file structure changed -imagemin and htmlmin installed

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 
 const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
 
 
 gulp.task('style', () => {
@@ -16,7 +19,7 @@ gulp.task('style', () => {
         autoprefixer({ cascade: false }),
         cssnano()
     ];
-    return gulp.src('./style.css')
+    return gulp.src('./src/style.css')
         .pipe(sourcemaps.init())
         .pipe(postcss(plugins))
         .pipe(sourcemaps.write())
@@ -25,8 +28,23 @@ gulp.task('style', () => {
 })
 
 
+gulp.task('imagemin', () => {
+    return gulp.src('./images/*')
+        .pipe(imagemin([
+            imagemin.mozjpeg({ quality: 75, progressive: true })
+        ]))
+        .pipe(gulp.dest('./images/'))
+});
+
+
+gulp.task('htmlmin', () => {
+    return gulp.src('./src/index.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("./build"))
+})
+
 gulp.task('javascript', () => {
-    return gulp.src('./index.js')
+    return gulp.src('./src/index.js')
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(sourcemaps.write())
@@ -37,18 +55,20 @@ gulp.task('javascript', () => {
 gulp.task('browsersync', () => {
     browserSync.init({
         server: {
-            baseDir: './'
+            baseDir: './src'
         }
     })
 
-    gulp.watch('./index.html').on('change', reload);
+    gulp.watch('./src/index.html').on('change', reload);
 
-    gulp.watch(['./style.css'], gulp.series('style'));
-    gulp.watch(['./index.js'], gulp.series('javascript'));
+    gulp.watch(['./src/style.css'], gulp.series('style'));
+    gulp.watch(['./src/index.js'], gulp.series('javascript'));
+    gulp.watch(['./src/index.html'], gulp.series('htmlmin'));
 });
 
 
 gulp.task('default', () => {
-    gulp.watch(['./style.css', './index.js'], gulp.series('browsersync'));
+    gulp.watch(['./src/style.css', './src/index.js'], gulp.series('browsersync'));
+    gulp.watch('./images/*', gulp.series('imagemin'));
 });
 
